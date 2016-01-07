f1fb2dd4e141369d056c1897c3ae2d5c349fcb92
privettoli <privettolisi>
2016-01-07 02:21:07 +0200

    Add Kovenant (asynchronous library for Kotlin with extensions for JavaFX)

--- src/main/kotlin/ua/kpi/databases/coursework/Application.kt
+++ src/main/kotlin/ua/kpi/databases/coursework/Application.kt
@@ -8,21 +8,23 @@ import javafx.scene.control.TextField
 import javafx.scene.image.Image
 import javafx.scene.layout.VBox
 import javafx.stage.Stage
-import javafx.stage.StageStyle
 
 class App : Application() {
     override fun start(stage: Stage) {
-        val editText = TextField("editText.text".i18n())
+        val editText = TextField().apply {
+            promptText = "editText.text".i18n()
+        }
         val completionsList = ListView<String>()
-
-        val vbox = VBox(editText, completionsList).apply {
+        val mainVBox = VBox(editText, completionsList).apply {
             spacing = 8.0
             padding = Insets(8.0)
         }
-        stage.title = "stage.title".i18n()
-        stage.icons.add(Image("/icon.png"))
-        stage.scene = Scene(vbox)
-        stage.show()
+        stage.apply {
+            title = "stage.title".i18n()
+            icons.add(Image("icon.png"))
+            scene = Scene(mainVBox)
+            show()
+        }
     }
 }
 
