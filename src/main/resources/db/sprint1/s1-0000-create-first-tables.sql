c4e10ef3d9b8080601aec8bbf14fcadb32f8b33b
privettoli <privettolisi>
2016-01-08 00:43:08 +0200

    Add Liquibase and first script

---
+++ src/main/resources/db/sprint1/s1-0000-create-first-tables.sql
@@ -0,0 +1,11 @@
+--liquibase formatted sql
+
+--changeset Anatolii_Papenko:PROJECT-0000
+CREATE TABLE `technological_map` (
+`id` INT UNSIGNED NOT NULL,
+`month` TINYINT UNSIGNED NOT NULL,
+`name` VARCHAR(512) NOT NULL,
+`fuel_consumption_liters` DECIMAL(7,2) UNSIGNED NOT NULL,
+`processing_time_weeks` DECIMAL(7,2) UNSIGNED NOT NULL,
+PRIMARY KEY (`id`));
+--rollback DROP TABLE `technological_map`;
