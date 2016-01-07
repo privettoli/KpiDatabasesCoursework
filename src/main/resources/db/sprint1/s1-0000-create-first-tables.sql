f8eebc68112701ab02b4b36886f94075adada547
privettoli <privettolisi>
2016-01-08 00:43:58 +0200

    Format SQL code

--- src/main/resources/db/sprint1/s1-0000-create-first-tables.sql
+++ src/main/resources/db/sprint1/s1-0000-create-first-tables.sql
@@ -1,11 +1,12 @@
---liquibase formatted sql
+-- liquibase formatted SQL
 
---changeset Anatolii_Papenko:PROJECT-0000
+-- changeset Anatolii_Papenko:PROJECT-0000
 CREATE TABLE `technological_map` (
-`id` INT UNSIGNED NOT NULL,
-`month` TINYINT UNSIGNED NOT NULL,
-`name` VARCHAR(512) NOT NULL,
-`fuel_consumption_liters` DECIMAL(7,2) UNSIGNED NOT NULL,
-`processing_time_weeks` DECIMAL(7,2) UNSIGNED NOT NULL,
-PRIMARY KEY (`id`));
---rollback DROP TABLE `technological_map`;
+  `id`                      INT UNSIGNED           NOT NULL,
+  `month`                   TINYINT UNSIGNED       NOT NULL,
+  `name`                    VARCHAR(512)           NOT NULL,
+  `fuel_consumption_liters` DECIMAL(7, 2) UNSIGNED NOT NULL,
+  `processing_time_weeks`   DECIMAL(7, 2) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`)
+);
+-- ROLLBACK DROP TABLE `technological_map`;
