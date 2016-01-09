544609b9f7b994ea6c850aaa90efba1336d15226
privettoli <privettolisi>
2016-01-10 01:31:15 +0200

    Added Kwery

---
+++ src/main/kotlin/ua/kpi/databases/coursework/Entities.kt
@@ -0,0 +1,12 @@
+package ua.kpi.databases.coursework
+
+/**
+ * Created by privettoli@gmail.com
+ * Here are listed entities of the project
+ */
+
+data class TechnologicalSmth(var id: Int,
+                            var name: String,
+                            var month: Byte,
+                            var fuelConsumptionLiters: Float,
+                            var processingTimeWeeks: Float)
