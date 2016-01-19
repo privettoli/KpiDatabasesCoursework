587be8a704373868ef7c73144253a23ed3ad4b45
privettoli <privettolisi>
2016-01-20 01:26:38 +0200

    Rewrote check to trigger for `late_passport_obtaining_chk`

--- src/main/resources/db/ddl/004-add-checks.sql
+++ src/main/resources/db/ddl/004-add-checks.sql
@@ -1,12 +1,21 @@
 -- liquibase formatted SQL
 
 -- changeset Roman_Metelyov:late_passport_obtaining_chk
-ALTER TABLE `national_passport_requests`
-ADD CONSTRAINT `late_passport_obtaining_chk` CHECK (`birth_certificate_id` IS NOT NULL AND
-                                                    ((EXTRACT(
-                                                        YEAR FROM FROM_DAYS(DATEDIFF(CURDATE(), (
-                                                          SELECT `birth_date`
-                                                          FROM `birth_certificates`
-                                                          WHERE `id` = `birth_certificate_id`)))
-                                                    )) <= 18 OR `pernalty_receipt_id` IS NOT NULL));
--- rollback ALTER TABLE `national_passport_requests` DROP CONSTRAINT `late_passport_obtaining_chk`;
+DELIMITER $$
+CREATE TRIGGER `late_passport_obtaining_chk` BEFORE INSERT ON `national_passport_requests`
+FOR EACH ROW
+  BEGIN
+    IF `birth_certificate_id` IS NULL OR
+       ((EXTRACT(
+           YEAR FROM FROM_DAYS(DATEDIFF(CURDATE(), (
+             SELECT `birth_date`
+             FROM `birth_certificates`
+             WHERE `id` = `birth_certificate_id`)))
+       )) > 18 AND `pernalty_receipt_id` IS NULL)
+    THEN
+      SIGNAL SQLSTATE '23001'
+      SET MESSAGE_TEXT = 'Late passport obtaining, you should pay your penalty';
+    END IF;
+  END$$
+DELIMITER ;
+-- rollback DROP TRIGGER IF EXISTS `late_passport_obtaining_chk`;
