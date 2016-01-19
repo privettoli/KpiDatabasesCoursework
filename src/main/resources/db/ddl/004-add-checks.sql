a2eb5a69b7f6cc751971530c395f85c133cce8e4
Roman_Metelov <roman-metelov@mail.ru>
2016-01-20 01:28:22 +0200

    Add NEW to the trigger

--- src/main/resources/db/ddl/004-add-checks.sql
+++ src/main/resources/db/ddl/004-add-checks.sql
@@ -5,13 +5,13 @@ DELIMITER $$
 CREATE TRIGGER `late_passport_obtaining_chk` BEFORE INSERT ON `national_passport_requests`
 FOR EACH ROW
   BEGIN
-    IF `birth_certificate_id` IS NULL OR
+    IF NEW.`birth_certificate_id` IS NULL OR
        ((EXTRACT(
            YEAR FROM FROM_DAYS(DATEDIFF(CURDATE(), (
              SELECT `birth_date`
              FROM `birth_certificates`
-             WHERE `id` = `birth_certificate_id`)))
-       )) > 18 AND `penalty_receipt_id` IS NULL)
+             WHERE `id` = NEW.`birth_certificate_id`)))
+       )) > 18 AND NEW.`penalty_receipt_id` IS NULL)
     THEN
       SIGNAL SQLSTATE '23001'
       SET MESSAGE_TEXT = 'Late passport obtaining, you should pay your penalty';
